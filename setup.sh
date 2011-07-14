560fda1fb5b5b0834ea6cb868593e9786a5f7b5b
marstall <marstall@gmail.com>
2011-07-14 18:53:11 -0400

    separated npm install from proxy config

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-sudo chgrp admin /dev/bpf*
-sudo chmod g+rw /dev/bpf*
+echo "installing npm ..."
+sudo curl http://npmjs.org/install.sh | sh
 
-sudo ipfw -q add 02000 fwd 127.0.0.1,3128 tcp from any to any dst-port 80 in recv en1
-sudo sysctl -w net.inet.ip.forwarding=1
-sudo sysctl -w net.inet.ip.fw.enable=1
-sudo sysctl -w net.inet.ip.fw.verbose=1
-sudo sysctl -w net.inet.ip.scopedroute=0
+echo "installing node.js packages ..."
+sudo npm install http-proxy
+sudo npm install socket-io
+sudo npm install ejs
+
+./configure_proxy.sh 
+
