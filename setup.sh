733d39c2f2575704ed916396cebb0794ab6fc0dc
Christopher Bowns <github@cbowns.com>
2012-09-12 10:43:14 -0700

    Properly check for, and set (if needed), the kernel flag for scoped routing.

--- setup.sh
+++ setup.sh
@@ -14,10 +14,19 @@ sudo cp /etc/sysctl.conf __sysctl.conf
 sudo cat _sysctl.conf>>__sysctl.conf
 sudo cp __sysctl.conf /etc/sysctl.conf
 
- echo '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC \
- "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist \
- version="1.0"><dict><key>Kernel \
- Flags</key><string>net.inet.ip.scopedroute=0</string></dict></plist>' > /Library/Preferences/SystemConfiguration/com.apple.Boot.plist
+kernelflags="net.inet.ip.scopedroute=0"
+
+currentflags=`sudo defaults read /Library/Preferences/SystemConfiguration/com.apple.Boot "Kernel Flags"`
+echo $currentflags
+
+if [[ "$currentflags" == "$kernelflags" ]]; then
+	echo "You're already set for scoped routing."
+else
+	sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.Boot "Kernel Flags" $kernelflags
+	echo "To use shim, you'll need to reboot just this once for the kernel flags change to take effect."
+fi
+
+sudo plutil -convert xml1 /Library/Preferences/SystemConfiguration/com.apple.Boot.plist
 
 ./configure_proxy.sh
 
